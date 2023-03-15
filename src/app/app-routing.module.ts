5168f4ae32ce4e0075a0a168a7b30df44feb6d69
~mikhers <~miguel.morenovuniagustiniana.edu.co>
2023-03-14 23:05:56 -0500

    useHash: true y routerLinkActive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,14 @@ import { EmpleadosComponent } from './pages/empleados/empleados.component';
 import { ProveedoresComponent } from './pages/proveedores/proveedores.component';
 
 const routes: Routes = [
-  {path: '', component: ProductosComponent},
+  {path: 'productos', component: ProductosComponent},
   {path: 'empleados', component: EmpleadosComponent},
   {path: 'proveedores', component: ProveedoresComponent},
   {path: '**', pathMatch: 'full', redirectTo: ''},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot( routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
