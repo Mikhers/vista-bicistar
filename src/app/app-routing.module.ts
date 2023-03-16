aa31d508ebfca1df91a8fc10c4904c7d6145a359
~mikhers <~miguel.morenovuniagustiniana.edu.co>
2023-03-15 22:59:57 -0500

    agregue animaciones a la App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { ProductosComponent } from './pages/productos/productos.component';
 import { EmpleadosComponent } from './pages/empleados/empleados.component';
 import { ProveedoresComponent } from './pages/proveedores/proveedores.component';
 import { SorryComponent } from './pages/sorry/sorry.component';
+import { ComprasComponent } from './pages/compras/compras.component';
 
 const routes: Routes = [
   {path: 'productos', component: ProductosComponent},
   {path: 'empleados', component: EmpleadosComponent},
   {path: 'proveedores', component: ProveedoresComponent},
+  {path: 'compras', component: ComprasComponent},
   {path: 'sorry', component: SorryComponent},
-  {path: '**', pathMatch: 'full', redirectTo: ''},
+  {path: '**', pathMatch: 'full', redirectTo: 'productos'},
 ];
 
 @NgModule({
