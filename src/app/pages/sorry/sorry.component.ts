2a653db38f9f9abb5b8844f2a1d83805c1e78c51
~mikhers <~miguel.morenovuniagustiniana.edu.co>
2023-03-16 09:11:36 -0500

    agregue la 'animacion en sorry (es un video)'

--- src/app/pages/sorry/sorry.component.ts
+++ src/app/pages/sorry/sorry.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-sorry',
   templateUrl: './sorry.component.html',
   styleUrls: ['./sorry.component.css']
 })
+
 export class SorryComponent {
-
-
-
 
 }
