b7dff6415fd3d05585fa7065f71d7b75dc1ac9ea
~mikhers <~miguel.morenovuniagustiniana.edu.co>
2023-03-30 18:52:56 -0500

    Punto de control (modal pedido producto casi terminada 'creo que la implementare en una vista hija')

--- src/app/pages/peddidos/peddidos.component.ts
+++ src/app/pages/peddidos/peddidos.component.ts
@@ -1,7 +1,7 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { EmpleadosInterface, ProveedorInterface, SedeInterface, productoInteface, PedidosInterface, PedidoProductoInterface } from '../../interfaces/bicistar-api.Interface';
 import { SedeService } from '../../services/sede.service';
-import { FormGroup, FormControl, Validators, FormArray, FormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { EmpleadosService } from '../../services/empleados.service';
 import { ProductoService } from '../../services/productos.service';
@@ -19,6 +19,7 @@ export class PeddidosComponent {
 
   searchTerm: string = '';
   titulo="Pedidos Bicistar"
+  infoProducto: any = {}
   showPut=false;
   maxToShow = 10;
   minToShow = 0;
@@ -26,7 +27,9 @@ export class PeddidosComponent {
   totalItems = 0
   ini = 1;
   fin = 7;
-  idEmpleado=0
+  totalPedido=0
+
+  horaActual!: string;
 
 
   sedes: SedeInterface[]=[];
@@ -34,7 +37,8 @@ export class PeddidosComponent {
   productos: productoInteface[]=[];
   proveedores: ProveedorInterface[]=[];
   pedidos: PedidosInterface[]=[];
-  productosForm: any[]=[];
+
+  productosForm: any[]=[];            //Pedidos-productos
 
   password: string="";
   passwordVisible: boolean = false;
@@ -68,30 +72,49 @@ export class PeddidosComponent {
 
     formPedido = new FormGroup({
       fecha_realizado: new FormControl(''),
-      fecha_llegada: new FormControl('', [Validators.required]),
-      estado_pedido: new FormControl('', [Validators.required]),
-      total_pedido: new FormControl('', [Validators.required]),
-      id_sede: new FormControl('', [Validators.required]),
-      id_proveedor: new FormControl('', [Validators.required]),
-      id_empleado: new FormControl('', [Validators.required])
-    })
-    formProductos = new FormGroup({
-      formProducto: new FormArray([], [Validators.required])
+      fecha_llegada: new FormControl('', [Validators.required]),//requerido
+      estado_pedido: new FormControl(''),//requerido
+      total_pedido: new FormControl(''),//requerido
+      id_sede: new FormControl('', [Validators.required]),//requerido
+      id_proveedor: new FormControl('', [Validators.required]),//requerido
+      id_empleado: new FormControl('', [Validators.required])//requerido
     })
 
-  // addProducto(): FormGroup{
-  //   return new FormGroup(
-  //     {
-  //       id_producto: new FormControl("", [Validators.required]),
-  //       cantidad_producto: new FormControl("", [Validators.required]),
-  //       precio_unitario:new FormControl("", [Validators.required])
-  //     }
-  //   )
-  // }
-  // asignarProducto():void{
-  //   const newProducto = this.formPedido.get('producto') as FormArray;
-  //   newProducto.push(this.addProducto());
-  // }
+
+    formProductos = new FormGroup({
+      id_producto: new FormControl('', [Validators.required]),
+      cantidad_producto: new FormControl('', [Validators.required]),
+      precio_unitario: new FormControl('', [Validators.required])
+    })
+
+  addProducto(): any{
+    const productoo: any =
+      {
+        id_producto: this.infoProducto.id_producto,
+        nombre_producto: this.infoProducto.nombre_producto,
+        cantidad_producto: this.formProductos.get('cantidad_producto')?.value,
+        precio_unitario: this.formProductos.get('precio_unitario')?.value
+      }
+      return productoo;
+  }
+  asignarProducto(){
+    const OBJETO = this.addProducto();
+    if(this.agregarObjeto({id_producto:OBJETO.id_producto, nombre_producto:OBJETO.nombre_producto})){
+      this.totalPedido = this.totalPedido + OBJETO.cantidad_producto * OBJETO.precio_unitario;
+      this.productosForm.push(OBJETO);
+      this.formProductos.reset();
+    }
+    else{
+      this.toastr.error('El producto ' + OBJETO.nombre_producto + ' ya existe en la lista.', "PRODUCTO YA FUE AGREGADO");
+    }
+  }
+  agregarObjeto(objeto: { id_producto: number, nombre_producto: string }): boolean {
+    const idExistente = this.productosForm.some(item => item.id_producto === objeto.id_producto);
+    if (!idExistente) {
+      return true
+    }
+    return false
+  }
 
   /*                                               PEDIDOS      GET    POST    PUT   DELETE                               */
 getPedido(){
@@ -105,15 +128,18 @@ getPedido(){
 }
 postPedido(){
   const now: string = new Date().toISOString();
+  console.warn(now)
   const PEDIDO: PedidosInterface = {
-    fecha_realizado: this.formPedido.get('fecha_realizado')?. value ?? "",
-    fecha_llegada: this.formPedido.get('fecha_llegada')?. value ?? now,
-    estado_pedido: this.formPedido.get('estado_pedido')?. value ?? "",
-    total_pedido: parseFloat(this.formPedido.get('total_pedido')?. value as string),
-    id_sede: parseInt(this.formPedido.get('id_sede')?. value as string),
-    id_proveedor: parseInt(this.formPedido.get('id_proveedor')?. value as string),
-    id_empleado: parseInt(this.formPedido.get('id_empleado')?. value as string)
+    fecha_realizado: now,
+    fecha_llegada: this.formPedido.get('fecha_llegada')?.value?.toString() ?? "",
+    estado_pedido: "en proceso",
+    total_pedido: this.totalPedido,
+    id_sede: parseInt(this.formPedido.get('id_sede')?.value as string),
+    id_proveedor: parseInt(this.formPedido.get('id_proveedor')?.value as string),
+    id_empleado: parseInt(this.formPedido.get('id_empleado')?.value as string)
   }
+  console.log(PEDIDO);
+  console.log(this.formPedido.get('fecha_realizado')?.value);
   this._pedido.postPedido(PEDIDO).subscribe(data=>{
     this.getPedido();
     this.closeModal();
@@ -124,7 +150,6 @@ postPedido(){
   })
 }
 
-arrarPedidoProducto: PedidoProductoInterface[]=[]
 getIdPedido(num:number){
   this._pedido.getIdPedido(num).subscribe((data:PedidosInterface)=>{
       this.formPedido.setValue({
@@ -160,6 +185,7 @@ putPedido(num:number){
     console.log(error)
   })
 }
+/*                                                               PEDIDO-PRODUCTO                                    */
 
   /*                                                               PROVEEDORES                                              */
   getProveedor(){
@@ -178,6 +204,15 @@ putPedido(num:number){
       this.itemsLista = Math.ceil(this.totalItems / 20 + 1);
     }, error => {
       this.toastr.error("Hubo un error inesperado en el sistema", 'ALGO SALIO MAL')
+      console.log(error)
+    })
+  }
+  getProducto(num:number){
+    this._producto.getIdProducto(num).subscribe(data=>{
+      console.log(data)
+      return data;
+    },error=>{
+      this.toastr.error("Hubo un error inesperado en el sistema", "ALGO SALIO MAL")
       console.log(error)
     })
   }
@@ -225,35 +260,34 @@ getSede(){
   @ViewChild('modal') modal!: ElementRef;
   openModal() {
     this.modal.nativeElement.style.display = 'block';
+    const fechaHora = new Date();
+    this.horaActual = fechaHora.toLocaleDateString() + ' ' + fechaHora.toLocaleTimeString();
+    this.getEmpleado()
+    this.getProveedor()
+    this.getSede()
   }
   closeModal() {
-      this.formPedido.reset()
-      this.modal.nativeElement.style.display = 'none';
-      this.showPut = false;
+    this.formPedido.reset()
+    this.formProductos.reset()
+    this.showPut = false;
+    this.totalPedido = 0;
+    this.productosForm = [];
+    this.modal.nativeElement.style.display = 'none';
   }
-  @HostListener('document:click', ['$event'])
-  onClick(event: MouseEvent) {
-    if (event.target === this.modal.nativeElement) {
-      this.closeModal();
-    }
-  }
+
 
 
   @ViewChild('modalProducto') modalProducto!: ElementRef;
   openModal2() {
     this.modalProducto.nativeElement.style.display = 'block';
+    this.getP()
   }
   closeModal2() {
-      this.formPedido.reset()
-      this.modalProducto.nativeElement.style.display = 'none';
+    this.modalProducto.nativeElement.style.display = 'none';
+    this.formProductos.reset()
       this.showPut = false;
   }
-  @HostListener('document:click', ['$event'])
-  onClick2(event: MouseEvent) {
-    if (event.target === this.modalProducto.nativeElement) {
-      this.closeModal2();
-    }
-  }
+
 
 
 }
